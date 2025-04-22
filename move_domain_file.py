c8f29c75a8d4f05b1ce003e012ed80cb33d71d00
Snigdha Ghosh Dastidar <snigdha.gdastidar@gmail.com>
2025-04-22 01:24:54 -0500

    game development

--- move_domain_file.py
+++ move_domain_file.py
@@ -4,7 +4,7 @@ import shutil
 # Configurations
 source_root = 'separated_output'
 destination_folder = 'output/model_output'
-target_domain = 'File_Handling'  # <-- Change domain here
+target_domain = 'Game_Development'  # <-- Change domain here
 
 # Ensure destination exists
 os.makedirs(destination_folder, exist_ok=True)
